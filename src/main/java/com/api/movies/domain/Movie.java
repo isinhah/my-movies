d1f3431f8e400ccd9f20c8d5fceeba8b87a57d8d
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-05-31 11:37:05 -0300

    mysql database

--- src/main/java/com/api/movies/domain/Movie.java
+++ src/main/java/com/api/movies/domain/Movie.java
@@ -3,6 +3,7 @@ package com.api.movies.domain;
 import com.api.movies.dtos.movies.MoviePostRequestBodyDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.Instant;
 @NoArgsConstructor
 @Data
 @Entity
+@Builder
 @Table(name = "tb_movie")
 public class Movie implements Serializable {
     @Id
